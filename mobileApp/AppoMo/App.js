64e0a2d0101bf0c1228e6eb482e4db30196eaedd
AnudhiDisara <121921613+AnudhiDisara@users.noreply.github.com>
2023-03-22 14:22:57 +0530

    Updated BrUpdate.js file

--- mobileApp/AppoMo/App.js
+++ mobileApp/AppoMo/App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.title}>Log into Appomo</Text>
+      </View>
+      <View styles={styles.inputtext}>
+        <TextInput placeholder="Username" />
+      </View>
+      <View styles={styles.inputtext}>
+        <TextInput placeholder="Password" />
+      </View>
+
+      <Button title='Submit' />
     </View>
+
+
+
+
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 40,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#108F94"
+
   },
+  title: {
+    color: "white",
+    fontSize: 40,
+    marginTop: 80,
+    marginBottom: 30
+  },
+
+  inputtext: {
+    borderColor: 'blue',
+    padding: 20,
+    marginBottom: 300,
+  }
+
+  
+
+
+
+
 });
