8d7ac1b81e33197917f5b8611488f70acd3a9002
GVGUththara <121917835+GVGUththara@users.noreply.github.com>
2023-03-30 15:00:16 +0530

    Customer Profile

--- Frontend/src/Pages/Termination/Termination.js
+++ Frontend/src/Pages/Termination/Termination.js
@@ -6,24 +6,24 @@ function Termination(){
     return(
         <div className={styles.mainContainer}>
             <Paper elevation={6} className={styles.paperDiv}>
-            <div className={styles.bodyMain}>
-                        <div className={styles.radioBoxContainer}>
-                            <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" >
-                                <FormControlLabel value="Completed" control={<Radio />} label="Completed" />
-                                <FormControlLabel value="NotCompleted" control={<Radio />} label="Not Completed" />
-                            </RadioGroup>
-                        </div>
-                        <div className={styles.buttonMain}>
-                            <Button variant="contained" sx={{mr:'10px'}}>Send message to the customer</Button>
-                        </div>
-                        <div className={styles.bodyTextbox}>
-                            <TextField id="outlined-basic" label="Total Amount : LKR " variant="outlined" sx={{ width: '100vw' }} />
-
-                        </div>                       
-                        <div className={styles.buttonMain}>
-                            <Button variant="contained" sx={{mr:'10px'}}>Generate Invoice</Button>
-                        </div>
+                <div className={styles.bodyMain}>
+                    <div className={styles.radioBoxContainer}>
+                        <label>Repairment&nbsp;</label>
+                        <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" >
+                            <FormControlLabel value="Completed" control={<Radio />} label="Completed" />
+                            <FormControlLabel value="NotCompleted" control={<Radio />} label="Not Completed" />
+                        </RadioGroup>
                     </div>
+                    <div className={styles.buttonMain}>
+                        <Button variant="contained" sx={{mr:'10px'}}>Send message to the customer</Button>
+                    </div>
+                    <div className={styles.bodyTextbox}>
+                       <TextField id="outlined-basic" label="Total Amount : LKR " variant="outlined" sx={{ width: '100vw' }} />
+                    </div>                       
+                    <div className={styles.buttonMain}>
+                            <Button variant="contained" sx={{mr:'10px'}}>Generate Invoice</Button>
+                    </div>   
+                </div>
             </Paper>
       </div>      
 
