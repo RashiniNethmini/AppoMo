35b5f23e705db1dbb945060c6545b4227fb890f3
BLPSankalpana <88890398+BLPSankalpana@users.noreply.github.com>
2023-06-24 19:14:28 +0530

    implement the backend part for product detail in App

--- mobileApp/AppoMo/Pages/ProductDEtails.js
+++ mobileApp/AppoMo/Pages/ProductDEtails.js
@@ -1,147 +1,160 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, StatusBar, Dimensions } from 'react-native';
-import { Text, Card,Button } from 'react-native-paper';
-import ModalDropdown from 'react-native-modal-dropdown';
+import React, { useState, useEffect } from 'react';
+//import { View, StyleSheet, StatusBar, Dimensions } from 'react-native';
+import axios from 'axios';
+import { Picker } from '@react-native-picker/picker';
+import { Text, Card,Button} from 'react-native-paper';
+import { View, StyleSheet, Platform, StatusBar, ScrollView, TouchableOpacity } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export default function ProductDetails() {
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [selectedModel, setSelectedModel] = useState(null);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('A');
+  const [models, setModels] = useState([]);
+  const [selectedModel, setSelectedModel] = useState('B');
 
-  const categories = [
-    { label: 'Laptop', value: 'laptop' },
-    { label: 'Computers', value: 'computers' },
-    { label: 'Mobile Phones', value: 'mobilephones' },
-    { label: 'Washing Machine', value: 'washingmachine' },
-    { label: 'Rice Cooker', value: 'ricecooker' },
-  ];
+  const [scrollEnabled, setScrollEnabled] = useState(true);
+  const [contentHeight, setContentHeight] = useState(0);
 
-  const models = {
-    laptop: ['Acer One 14 Business Laptop', 'ALenovo E41-55 AMD Laptop', 'Asus VivoBook 15'],
-    computers: ['Dell. XPS Desktop', 'MSI. MEG Trident X', 'Apple. Mac Mini M2 (2023)'],
-    mobilephones: ['Huawei Y9 Prime', 'Samsung Galaxy', 'iPhone 12'],
-    washingmachine: ['10.5kg, AI Direct Drive ', '8kg, AI Direct Drive Front Load', '7kg, 6 Motion Inverter Direct Drive Front Load'],
-    ricecooker: ['Panasonic SR-DF101', 'Panasonic SR-DF181 White Microcomputer', 'AROMA® Professional 8-Cup (Cooked)'],
+  useEffect(() => {
+    
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get('http://192.168.1.226:8070/Product/first'); 
+      const product = response.data;
+      const availableCategories = Object.keys(product).filter(category => Array.isArray(product[category]) && product[category].length > 0);
+      setCategories(availableCategories);
+      if (availableCategories.length > 0) {
+        setSelectedCategory(availableCategories[0]);
+        fetchModels(availableCategories[0]);
+      }
+    } catch (error) {
+      console.error('Failed to fetch categories:', error);
+    }
   };
 
-  const handleCategoryChange = (value) => {
-    setSelectedCategory(value);
-    setSelectedModel(null);
+  const fetchModels = async (category) => {
+    try {
+      const response = await axios.get('http://192.168.1.226:8070/Product/first'); 
+      const product = response.data;
+      const availableModels = product[category] || [];
+      setModels(availableModels);
+      if (availableModels.length > 0) {
+        setSelectedModel(availableModels[0]);
+      }
+    } catch (error) {
+      console.error('Failed to fetch models:', error);
+    }
   };
 
-  const handleModelChange = (value) => {
-    setSelectedModel(value);
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+    fetchModels(category);
   };
 
-  const screenWidth = Dimensions.get('window').width;
-  const cardWidth = screenWidth - 40; // Subtracting padding on both sides
+  const handleModelChange = (model) => {
+    setSelectedModel(model);
+  };
 
   return (
     <>
-      <StatusBar backgroundColor="#108F94" translucent={true} />
-      <View style={styles.container}>
-        <Text variant="headlineLarge" style={styles.title}>
-          Our Products
-        </Text>
+     <StatusBar backgroundColor="#108F94" translucent={true} />
+    <View>
+    <View>
+        <Text variant="headlineLarge" style={styles.title} >Our Products</Text>
+    </View>
+    <KeyboardAwareScrollView
+        style={styles.view}
+        contentContainerStyle={styles.contentContainer}
+        scrollEnabled={scrollEnabled}
+        keyboardShouldPersistTaps="always"
+      >
+         <View onLayout={e => setContentHeight(e.nativeEvent.layout.height)}>
+         <View >
+            <Card style={styles.card}>
+              <Card.Content style={styles.cardContent}>
+                <Card.Actions>
+                  <ScrollView>
 
-        <Card style={[styles.card, { width: cardWidth }]}>
-          <Card.Content style={styles.cardContent}>
-            <View style={styles.inputContainer}>
-              <Text style={styles.inputLabel}>Category:</Text>
-              <ModalDropdown
-                options={categories.map((category) => category.label)}
-                onSelect={(index) => handleCategoryChange(categories[index].value)}
-                defaultValue="Select a category"
-                style={styles.dropdown}
-                textStyle={styles.dropdownText}
-                dropdownStyle={styles.dropdownOptions}
-                dropdownTextStyle={styles.dropdownOptionText}
-              />
-            </View>
 
-            <View style={styles.inputContainer}>
-              <Text style={styles.inputLabel}>Model:</Text>
-              <ModalDropdown
-                options={selectedCategory ? models[selectedCategory] : []}
-                onSelect={(index) => handleModelChange(models[selectedCategory][index])}
-                defaultValue="Select a model"
-                style={styles.dropdown}
-                textStyle={styles.dropdownText}
-                dropdownStyle={styles.dropdownOptions}
-                dropdownTextStyle={styles.dropdownOptionText}
-                disabled={!selectedCategory}
-              />
-            </View>
 
-            {selectedModel && (
-              <Text style={styles.selectedText}>
-                Selected: {selectedCategory} - {selectedModel}
-              </Text>
-            )}
-          </Card.Content>
-        </Card>
-        <View>
-                    
-                    <Button mode="contained"
-                    //  onPress={()=>handleSubmit()} 
-                     style={{ backgroundColor: '#388F82' }} 
-                     >
-                      OK
-                    </Button>
-                 
-                  </View>
+      <Text>Category:</Text>
+      <Picker
+        selectedValue={selectedCategory}
+        onValueChange={handleCategoryChange}
+      >
+        {categories.map(category => (
+          <Picker.Item key={category} label={category} value={category} />
+        ))}
+      </Picker>
+
+      <Text>Model:</Text>
+      <Picker
+        selectedValue={selectedModel}
+        onValueChange={handleModelChange}
+      >
+        {models.map(model => (
+          <Picker.Item key={model} label={model} value={model} />
+        ))}
+      </Picker>
+      
+
+
+
+
+
+
+</ScrollView>
+
+</Card.Actions>
+</Card.Content>
+</Card>
+<View>
+          <Button mode="contained" style={{ backgroundColor: '#388F82' }}>
+            OK
+          </Button>
+        </View>
+
+</View>
       </View>
+      
+      </KeyboardAwareScrollView>
+    </View>
+    
     </>
   );
-}
-
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 60,
-      padding: 10,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      textAlign: 'center',
-      marginBottom: 10,
-      color: '#FFFFFF',
-      fontWeight: 'bold',
-    },
-    cardContent: {
-      alignItems: 'center',
-    },
-    dropdown: {
-      height: 40,
-      width: '100%',
-      marginTop: 10,
-      borderWidth: 1,
-      borderColor: '#999999',
-      borderRadius: 4,
-      paddingHorizontal: 10,
-      justifyContent: 'center',
-    },
-    dropdownText: {
-      fontSize: 16,
-      color: '#333333',
-    },
-    dropdownOptions: {
-      marginTop: 10,
-    },
-    modelDropdownContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginTop: 10,
-    },
-    modelDropdownLabel: {
-      marginRight: 10,
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-    selectedText: {
-      marginTop: 10,
-      textAlign: 'center',
-    },
-  });
-  
+ 
+  title: {
+    textAlign: 'center',
+    marginTop: 100, 
+    color: '#ffff',
+    fontWeight: 'bold',
+    marginBottom:1
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    width: '100%',
+  },
+  cardContent: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  card: {
+    marginTop: 1, // Adjust this value to change the space between title and card
+    paddingVertical: 10,
+    marginHorizontal: 5,
+    width: 300,
+    marginBottom:250
+  },
+
+
+});
